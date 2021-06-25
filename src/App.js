a580fa72cd220bf56ecbcdd0c64eb6678c429287
mingyeongho <1736s@naver.com>
2021-06-26 04:22:52 +0900

    history

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Link, Route } from "react-router-dom";
 import "./App.css";
+import About from "./Components/router/About";
+import HistorySample from "./Components/router/HistorySample";
+import Home from "./Components/router/Home";
 
 function App() {
-  return <div className="App">asd</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/history" component={HistorySample} />
+    </div>
+  );
 }
 
 export default App;
