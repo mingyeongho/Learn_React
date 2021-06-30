54258daa171bf7cda4320b044dd55401867363f7
mingyeongho <1736s@naver.com>
2021-06-30 23:04:27 +0900

    pagination을 위한 초석

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import { Link, Route } from "react-router-dom";
+import { useCallback, useState } from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
-import About from "./Components/router/About";
-import HistorySample from "./Components/router/HistorySample";
-import Home from "./Components/router/Home";
-import Profile from "./Components/router/Profile";
+import Categories from "./Components/News/Categories";
+import NewsList from "./Components/News/NewsList";
 
-function App() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/history">History 예제</Link>
-        </li>
-        <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
-        </li>
-      </ul>
-      <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
-      <Route path="/history" component={HistorySample} />
-    </div>
-  );
-}
+const App = () => {
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return <Route path="/:category?" component={NewsPage} />;
+};
 
 export default App;
