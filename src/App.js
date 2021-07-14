409e2a776c54df91e85c3c2cf150663aeeeabe82
mingyeongho <1736s@naver.com>
2021-07-15 02:55:11 +0900

    Code Spliting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
+import { useState } from "react";
 import "./App.css";
+import loadable from "@loadable/component";
+
+// React.lazy: 컴포넌트를 렌더링하는 시점에서 비동기적으로 로딩할 수 있게 해주는 유틸 함수
+// const SplitMe = React.lazy(() => import("./Components/CodeSpliting/SplitMe"));
+
+const SplitMe = loadable(() => import("./Components/CodeSpliting/SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 const App = () => {
+  const [visiable, setVisiable] = useState(false);
+  const onClick = () => {
+    setVisiable(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div>
       <header>
-        <p>Header</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React
+        </p>
+        {visiable && <SplitMe />}
       </header>
     </div>
   );
